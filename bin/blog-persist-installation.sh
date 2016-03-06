03cb91665ebb23b67f247af7d4e8dcba75d5916c
Attila Szeremi <attila@szeremi.org>
2016-03-06 16:33:47 +0000

    Use more-or-less working blog install scripts.
    Sudo may be needed for the bin/blog-persist-install.sh script

--- bin/blog-persist-installation.sh
+++ bin/blog-persist-installation.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Persist the installation for Drupal so that new instances of szeremi-blog would always use it.
-docker cp szeremi-blog-install:/var/www/html/sites/default/settings.php /mnt/szeremi-blog/sites/default/settings.php
+docker cp szeremi-blog-install:/var/www/html/sites /mnt/szeremi-blog/sites
