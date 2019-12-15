4b129da06c75a28de8c0ba3ddac25d980151f879
Attila Szeremi <attila@szeremi.org>
2019-12-15 17:24:09 +0100

    Adjust lycee-overture.sh

--- bin/lycee-overture.sh
+++ bin/lycee-overture.sh
@@ -5,9 +5,9 @@
 source /etc/environment
 
 docker run \
+  --rm \
   -v ${LYCEE_OVERTURE_STORAGE_DIR}:/var/www/storage \
   -v ${LYCEE_OVERTURE_NGINX_CONFIG_FILE}:/etc/nginx/sites-available/default \
   --env-file=${LYCEE_OVERTURE_ENV_FILE} \
-  -e LOG_CHANNEL=errorlog \
   --network szeremi \
   "$@"
