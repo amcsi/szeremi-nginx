54e5225be7cdd79902313afbc085836d12139236
Attila Szeremi <attila@szeremi.org>
2020-02-16 13:15:59 +0100

    Use secret subdomain for jenkins-proxy

--- projects/jenkins-proxy/start.sh
+++ projects/jenkins-proxy/start.sh
@@ -3,9 +3,9 @@
 /usr/bin/docker run --name $1 \
   -p 10096:8080 \
   -e PROXY_TO_HOST_IP_PORT=$(/sbin/ip -4 addr show docker0 | grep -Po 'inet \K[\d.]+'):8080 \
-  -e VIRTUAL_HOST=jenkins.szeremi.org,jenkins.localtest.me \
+  -e VIRTUAL_HOST=jejenkins2.szeremi.org,jenkins.localtest.me \
   -e VIRTUAL_PORT=8080 \
-  -e LETSENCRYPT_HOST=jenkins.szeremi.org \
+  -e LETSENCRYPT_HOST=jejenkins2.szeremi.org \
   -e LETSENCRYPT_EMAIL=attila@szeremi.org \
   --network szeremi \
   $2
